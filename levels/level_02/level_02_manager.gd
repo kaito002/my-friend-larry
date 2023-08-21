dd4e1bc6e82c8b5c8a9585a8e7a3a4421e38a473
Carlos Montes <carlosmontes002@gmail.com>
2023-08-20 21:44:43 -0300

    create level 02

---
+++ levels/level_02/level_02_manager.gd
@@ -0,0 +1,18 @@
+extends Node3D
+
+@onready var purple_button: WorldButton = $purple_button
+@onready var especial_door_animator: AnimationPlayer = $especial_door/door_animator
+@onready var pause_menu: PauseMenu = $pause_menu
+@onready var navigator: Navigator = get_tree().root.get_node('/root/main/navigator')
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	purple_button.connect('pressed', func (): especial_door_animator.play("open"))
+	purple_button.connect('released', func (): especial_door_animator.play_backwards("open"))
+	pause_menu.connect('menu_showed', func(): Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE))
+	pause_menu.connect('menu_hided', func(): Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED))
+	pause_menu.connect('restart', _restart_level)
+	
+func _restart_level():
+	var level_02 = load('res://levels/level_02/level_02.tscn').instantiate()
+	navigator.replace(level_02)
--- main/main_menu.gd
+++ main/main_menu.gd
@@ -4,10 +4,12 @@ class_name MainMenu
 @onready var navigator: Navigator = get_tree().root.get_node('/root/main/navigator')
 @onready var tutorial_btn: TextureButton = $ui/tutorial_btn
 @onready var level_01_btn: TextureButton = $ui/level_01_btn
+@onready var level_02_btn: TextureButton = $ui/level_02_btn
 
 func _ready():
-	level_01_btn.connect('pressed', _go_to_level_01)
 	tutorial_btn.connect('pressed', _go_to_tutorial)
+	level_01_btn.connect('pressed', _go_to_level_01)
+	level_02_btn.connect('pressed', _got_to_level_02)
 	
 func _go_to_tutorial():
 	var tutorial = load("res://levels/tutorial/tutorial.tscn").instantiate()
@@ -16,3 +18,7 @@ func _go_to_tutorial():
 func _go_to_level_01():
 	var level_01 = load("res://levels/level_01/level_01.tscn").instantiate()
 	navigator.replace(level_01)
+	
+func _got_to_level_02():
+	var level_02 = load("res://levels/level_02/level_02.tscn").instantiate()
+	navigator.replace(level_02)
